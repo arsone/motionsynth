63b3876fc6ec47331d7720b8185f6e44dc3fb16d
Adam <adsalberg@gmail.com>
2015-02-11 12:11:18 -0500

    Initial Commit

---
+++ motionsynth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  motionsynth
+//
+//  Created by Adam Salberg on 2/11/15.
+//  Copyright (c) 2015 Adam Salberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
